8c6afeabc9d67ec2eabcbadafcd65fe6a4b02e06
ding <zuosheng.ding@adesso.de>
2024-12-30 10:32:23 +0100

    update angular/material version to 15

--- frontend/src/app/layouts/full/header/header.component.ts
+++ frontend/src/app/layouts/full/header/header.component.ts
@@ -1,5 +1,5 @@
 import { Component } from '@angular/core';
-import { MatDialog, MatDialogConfig } from '@angular/material/dialog';
+import { MatLegacyDialog as MatDialog, MatLegacyDialogConfig as MatDialogConfig } from '@angular/material/legacy-dialog';
 import { Router } from '@angular/router';
 
 @Component({
